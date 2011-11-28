48952650c7b51897472c46efd7c611d08a048d5c
Guchko Gleb <guchko.gleb@gmail.com>
2011-11-28 18:57:48 +0400

    1

--- js/custom.js
+++ js/custom.js
@@ -4,30 +4,44 @@
 
 
 // cufon font replacement class/id
-Cufon.replace('h1,h2,h3,h4,th',{textShadow: '0px 1px 0px #ffffff'});
+Cufon.replace('h1,h2,h3,h4,th', {textShadow: '0px 1px 0px #ffffff'});
 
- 
-$(document).ready(function(){
 
-$('#navigation').localScroll({
-    offset: { top : '-40px' }
-});
+$(document).ready(function() {
+
+    $('#navigation').localScroll({
+        offset: { top : '-40px' }
+    });
+
+
+    $('#defaultCountdown').countdown({
+        until: new Date(2012, 2 - 1, 18),
+        format: 'DHMS',
+        layout:
+            '<div id="vals">' +
+                '<div id="d" class="numbers">{dnn}</div>' +
+                '<div id="h" class="numbers">{hnn}</div>' +
+                '<div id="m" class="numbers">{mnn}</div>' +
+                '<div id="s" class="numbers">{snn}</div>' +
+
+                '</div>'
+    });
 
 
 //Nivo slider
-$('#slider').nivoSlider({
+    $('#slider').nivoSlider({
         effect:'random', // Specify sets like: 'fold,fade,sliceDown' or choose 'random' for mixed effects
         slices:15, // For slice animations
         boxCols: 8, // For box animations
         boxRows: 4, // For box animations
         animSpeed:500, // Slide transition speed
         pauseTime:5000, // How long each slide will show
-		pauseOnHover:true,
-       directionNav:true, // Next & Prev navigation
+        pauseOnHover:true,
+        directionNav:true, // Next & Prev navigation
         directionNavHide:true, // Only show on hover
         controlNav:false // 1,2,3... navigation
-	
-});
+
+    });
 
 ////Fancybox for image / partner gallery
 //$("a[rel=next]").fancybox({
@@ -41,27 +55,27 @@ $('#slider').nivoSlider({
 //});
 
 //On Hover Event for gallery, social
-$('ul.gallery li img, ul.social li img').hover(function(){
-			$(this).animate({opacity: 0.6}, 300);
-		}, function () {
-			$(this).animate({opacity: 1}, 300);
-});
+    $('ul.gallery li img, ul.social li img').hover(function() {
+        $(this).animate({opacity: 0.6}, 300);
+    }, function () {
+        $(this).animate({opacity: 1}, 300);
+    });
 
 //subscribe form
-$(function(){
-	$('#subform').validate({
-	    submitHandler: function(form) {
-			    $(form).ajaxSubmit({
-				    url: 'http://tanshcreative.com/eventus-lp-preview/php/subscribe-form.php',
-					clearForm: true,
-				    success: function() {
-				    	$('.sub_inner').hide();
-				    	$('.subscribe').append("<h3>Thank you!</h3><p>Duis aute habent claritatem irure dolor in habent reprehenderit qui in voluptate quod velit esse cillum soluta dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>")
-				    }
-			    });
-		    }
-	});         
-});
+    $(function() {
+        $('#subform').validate({
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    url: 'http://tanshcreative.com/eventus-lp-preview/php/subscribe-form.php',
+                    clearForm: true,
+                    success: function() {
+                        $('.sub_inner').hide();
+                        $('.subscribe').append("<h3>Thank you!</h3><p>Duis aute habent claritatem irure dolor in habent reprehenderit qui in voluptate quod velit esse cillum soluta dolore eu fugiat nulla pariatur. Excepteur sint occaecat.</p>")
+                    }
+                });
+            }
+        });
+    });
 
 
 }); // close document.ready
